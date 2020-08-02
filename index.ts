3860b7dc0799c19fda250ecab1383eb51bbfd697
Anwesh43 <anweshthecool0@gmail.com>
2020-08-02 23:24:54 +0530

    adding MRLNode class

--- index.ts
+++ index.ts
@@ -139,3 +139,45 @@ class Animator {
         }
     }
 }
+
+class MRLNode {
+
+    next : MRLNode 
+    prev : MRLNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new MRLNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawMRLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : MRLNode {
+        var curr : MRLNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
